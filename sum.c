59f28586e792a67c53d177c872f1403d4ab16fb1
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2025-01-22 16:26:13 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+float main(){
+  float number1,number2;
+  float sum;
+  printf("\nEnter two floating numbers");
+  scanf("%f%f",&number1,&number2);
+  sum=number1+number2;
+  printf("%f+%f=%f\n",number1,number2,sum);
+  
+  
+  return 0;
+}
+  

6bf3bed853dbd6e416bcb1e916bf6768fae26ff9
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2025-02-16 12:49:48 +0530

    pgm to swap two numbers

---
+++ Swapping.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,temp;
+    printf("Enter first number:");
+    scanf("%d",&num1);
+    printf("Enter second number:");
+    scanf("%d",&num2);
+    temp=num1;
+    num1=num2;
+    num2=temp;
+    printf("num1:%d",num1);
+    printf("\nnum2:%d",num2);
+    return 0;
+
+
+}

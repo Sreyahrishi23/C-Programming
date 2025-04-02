cca3261c2731c60266c45fe28c0adbcf426c55a6
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2025-04-02 14:53:03 +0530

    to list full details of students using "struct"

--- struct1.c
+++ struct1.c
@@ -6,10 +6,11 @@ struct student{
 }  
   main(){
   struct student s1;
+  printf("\nEnter the name:");
+  fgets(s1.name,sizeof(s1.name),stdin); 
   printf("\nEnter the roll number:");
   scanf("%d",&s1.rollNo);
-  printf("\nEnter the name:");
-  scanf("%s",s1.name);
+  
   printf("\nEnter the CGPA:");
   scanf("%f",&s1.cgpa);
   printf("\nEnter the student details");

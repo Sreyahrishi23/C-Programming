b7bf5515816f99036fd46bda6bc549369ea05045
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2025-04-08 10:37:11 +0530

    to print multiples characters

---
+++ multchar2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  FILE *fp;
+  char ch;
+  fp=fopen("file.txt","w");
+  while(ch=getc(stdin)!='z'){
+    putc(ch,fp);
+  }
+  fclose(fp);
+  fp=fopen("input.txt","r");
+  while((ch=getc(fp))!=EOF){
+    printf("%c",ch);
+  }
+  fclose(fp);
+  return 0;

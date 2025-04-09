79b107a105494ea2e9961e4ee0e643397bb1719c
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2025-04-09 14:10:30 +0530

    newdoc.c

--- newdoc.c
+++ newdoc.c
@@ -3,10 +3,11 @@ int main(){
   FILE *fp1,fp2,fp3;
   fp1=fopen("number.txt","r");
   int num;
+  fscanf(fp1,"%d",&num);
   while(!feof(fp1)){
-    fscanf(fp1,"%d",&num);
     printf("%d\t",num);
+    fscanf(fp1,"%d",&num);
   }
   fclose(fp1);
   return 0;
-}  
+}

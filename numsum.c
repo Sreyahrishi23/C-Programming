03660694bfd7b0376f77e6831e0bc2993464bac0
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2025-04-15 14:05:09 +0530

    numsum.c
    to find sum of two numbers

---
+++ numsum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int sum,num1,num2;
+    printf("Enter first number:\n");
+    scanf("%d",&num1);
+    printf("Enter second number:\n");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("The sum is %d",sum);
+}

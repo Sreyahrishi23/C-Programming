aa8cadf20961b048065e5bc77120f595224a9ba1
Sreyahrishi23 <sreyahrishikesh2028@cs.sjcetpalai.ac.in>
2025-04-30 16:10:19 +0530

    using ptr

---
+++ ptr.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+typedef struct{
+  int rollno;
+  char name[20];
+}student;
+int main(){
+  student s1;
+  student *ptr;
+  ptr=&s1;
+  ptr->rollno=10;
+  printf("%d",ptr->rollno);
+  return 0;
+}  
